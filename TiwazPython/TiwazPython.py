b28a9768b29393de68044f7f6f67928bf360c529
Normandy123 <35195956+Normandy123@users.noreply.github.com>
2018-03-11 18:38:46 +0100

    update

--- TiwazPython/TiwazPython.py
+++
@@ -1,9 +0,0 @@
-import tiwaz_cython
-
-tiwaz_cython.run_engine()
-
-x = tiwaz_cython.PyMeshComponent()
-
-del x
-
-tiwaz_cython.exit_engine()
--- x64/Release/TiwazPython.py
+++
@@ -1,7 +0,0 @@
-import tiwaz_cython
-
-tiwaz_cython.run_engine()
-
-x = tiwaz_cython.PyEngineObject()
-
-tiwaz_cython.exit_engine()
